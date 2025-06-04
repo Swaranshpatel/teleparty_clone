df1dbf43ddbcfb26f8f4bda335ced3b7a760e534
Swaranshpatel <99585835+Swaranshpatel@users.noreply.github.com>
2025-06-05 02:26:40 +0530

    .

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const io = require("socket.io")(3000, {
-    cors: { origin: "*" }
+const PORT = process.env.PORT || 3000;
+const io = require("socket.io")(PORT, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.roomId = roomId;
   });
-  
-  io.on("connection", (socket) => {
-    socket.on("join-room", (roomId) => {
-      socket.join(roomId);
-      socket.roomId = roomId;
-    });
-  
-    socket.on("video-event", (data) => {
-      const room = socket.roomId;
-      if (room) {
-        socket.to(room).emit("video-event", data);
-      }
-    });
+
+  socket.on("video-event", (data) => {
+    const room = socket.roomId;
+    if (room) {
+      socket.to(room).emit("video-event", data);
+    }
   });
+});
